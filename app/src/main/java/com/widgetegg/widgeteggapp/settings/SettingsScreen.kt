ca948b4240d06f86a21163e37e498b196206697a
mvdiener <mvdiener@gmail.com>
2024-10-10 17:28:18 -0700

    Add warning and dialog for battery permissions in the settings screen

--- app/src/main/java/com/widgetegg/widgeteggapp/settings/SettingsScreen.kt
+++ app/src/main/java/com/widgetegg/widgeteggapp/settings/SettingsScreen.kt
@@ -1,5 +1,10 @@
 package com.widgetegg.widgeteggapp.settings
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.PowerManager
+import android.provider.Settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -15,18 +20,27 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.widgetegg.widgeteggapp.Routes
@@ -46,6 +60,15 @@ fun SettingsScreen(navController: NavController) {
         settingsViewModel.updateShowFuelingShip(preferences.getShowFuelingShip())
         settingsViewModel.updateShowTargetArtifactLargeWidget(preferences.getTargetArtifactLargeWidget())
         settingsViewModel.updateShowTankLevels(preferences.getShowTankLevels())
+    }
+
+    val packageName = context.packageName
+    val pm: PowerManager =
+        context.getSystemService(Context.POWER_SERVICE) as PowerManager
+    settingsViewModel.updateIsOptimizationDisabled(pm.isIgnoringBatteryOptimizations(packageName))
+
+    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
+        settingsViewModel.updateIsOptimizationDisabled(pm.isIgnoringBatteryOptimizations(packageName))
     }
 
     Column(
@@ -73,9 +96,96 @@ fun SettingsScreen(navController: NavController) {
     ) {
         Text(text = "Settings", fontSize = TextUnit(24f, TextUnitType.Sp))
 
+        if (!settingsViewModel.isOptimizationDisabled) {
+            BatteryPermissionsContent(settingsViewModel, packageName, context)
+        }
+
         AllWidgetsGroup(settingsViewModel)
         NormalWidgetGroup(settingsViewModel)
         LargeWidgetGroup(settingsViewModel)
+    }
+}
+
+@Composable
+fun BatteryPermissionsContent(
+    settingsViewModel: SettingsViewModel,
+    packageName: String,
+    context: Context
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 15.dp, top = 15.dp)
+            .clickable {
+                settingsViewModel.updateShowBatteryOptimizationDialog(true)
+            },
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            Icons.Rounded.Warning,
+            contentDescription = "Battery permissions warning",
+            modifier = Modifier
+                .padding(end = 5.dp)
+                .size(35.dp),
+            tint = Color.Yellow
+        )
+        Text(text = "Battery Optimization Enabled", color = Color.White)
+        Icon(
+            Icons.Rounded.Info,
+            contentDescription = "Battery optimization info",
+            modifier = Modifier
+                .padding(start = 5.dp)
+                .size(15.dp)
+        )
+        BatteryPermissionsDialog(settingsViewModel, packageName, context)
+    }
+}
+
+@Composable
+fun BatteryPermissionsDialog(
+    settingsViewModel: SettingsViewModel,
+    packageName: String,
+    context: Context
+) {
+    if (settingsViewModel.showBatteryOptimizationDialog) {
+        Dialog(
+            onDismissRequest = {
+                settingsViewModel.updateShowBatteryOptimizationDialog(false)
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = MaterialTheme.colorScheme.surfaceContainer,
+                        shape = RoundedCornerShape(size = 16.dp)
+                    )
+                    .padding(20.dp)
+            ) {
+                Text(
+                    text =
+                    """
+                        The widgets attempt to update automatically in the background every 15 minutes. In order to do this, they run background processes to fetch mission data.     
+                        
+                        Battery optimization can prevent these processes from running. If you notice issues with widget updates, it is recommended that you turn off battery optimization for this app.
+                    """.trimIndent()
+                )
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 5.dp),
+                    onClick = {
+                        settingsViewModel.updateShowBatteryOptimizationDialog(false)
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        intent.data = Uri.parse("package:$packageName")
+                        context.startActivity(intent)
+                    }
+                ) {
+                    Text(text = "App Settings")
+                }
+            }
+        }
     }
 }
 
