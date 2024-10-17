cc02a8c494ed69a9c6075d394cac4e641034587d
mvdiener <mvdiener@gmail.com>
2024-10-16 23:50:43 -0700

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.widgetegg.widgeteggapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         ndk {
