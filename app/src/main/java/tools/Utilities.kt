af91ada95462e4e9cea89bee03c1d19b33dbc9a6
mvdiener <mvdiener@gmail.com>
2025-04-23 23:01:07 -0700

    Account for private farms and departed users

--- app/src/main/java/tools/Utilities.kt
+++ app/src/main/java/tools/Utilities.kt
@@ -12,10 +12,14 @@ import android.net.Uri
 import android.provider.CalendarContract
 import android.provider.CalendarContract.Reminders
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.scale
+import androidx.core.graphics.toColorInt
 import data.ALL_SHIPS
 import data.CalendarEntry
 import data.ContractData
 import data.ContractInfoEntry
+import data.ContributorInfoEntry
 import data.FuelLevelInfo
 import data.GoalInfoEntry
 import data.MissionData
@@ -30,10 +34,6 @@ import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 import java.util.TimeZone
-import androidx.core.graphics.toColorInt
-import androidx.core.graphics.createBitmap
-import androidx.core.graphics.scale
-import data.ContributorInfoEntry
 
 fun getMissionPercentComplete(
     missionDuration: Double,
@@ -153,12 +153,15 @@ fun formatContractData(contractInfo: ContractData): List<ContractInfoEntry> {
             }
 
         var formattedContributors: List<ContributorInfoEntry> = emptyList()
-        status?.contributorsList?.forEach { contributor ->
+        status?.contributorsList?.filterNot { contributor ->
+            // Remove any [departed] users stuck from contract creation
+            contributor.userName == "[departed]" && contributor.contributionAmount == 0.0 && contributor.contributionRate == 0.0 && contributor.uuid.isNullOrEmpty()
+        }?.forEach { contributor ->
             formattedContributors = formattedContributors.plus(
                 ContributorInfoEntry(
                     eggsDelivered = contributor.contributionAmount,
                     eggRate = contributor.contributionRate,
-                    offlineTime = contributor.farmInfo.timestamp
+                    offlineTime = contributor.farmInfo?.timestamp ?: 0.0
                 )
             )
         }
