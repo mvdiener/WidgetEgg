d05f76d09e56e3584e01255ef285ae087352658a
mvdiener <mvdiener@gmail.com>
2025-05-12 18:09:42 -0700

    Update ktor libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     id("com.google.protobuf")
     kotlin("plugin.serialization") version "2.0.20"
 }
