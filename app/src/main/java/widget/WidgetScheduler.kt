47ae2516ce3c0f94765629c586a9f6265219c98d
mvdiener <mvdiener@gmail.com>
2025-06-04 19:46:35 -0700

    Small refactor

--- app/src/main/java/widget/WidgetScheduler.kt
+++ app/src/main/java/widget/WidgetScheduler.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 class WidgetScheduler() {
     fun scheduleUpdate(context: Context) {
-        cancelOldWork(context)
+        cancelOldWorkers(context)
         // Update widget every 15 minutes
         val workRequest = PeriodicWorkRequestBuilder<WidgetWorker>(15, TimeUnit.MINUTES)
             .setBackoffCriteria(BackoffPolicy.LINEAR, 45, TimeUnit.SECONDS)
@@ -23,20 +23,15 @@ class WidgetScheduler() {
 
     // The worker(s) have been renamed, added and removed over time
     // Just in case any old workers are running on a user's device, get rid of them
-    private fun cancelOldWork(context: Context) {
+    private fun cancelOldWorkers(context: Context) {
         val workManager = WorkManager.getInstance(context)
+        val oldWorkNames = listOf("mission_widget_worker", "contract_widget_worker")
 
-        val oldMissionWorkerName = "mission_widget_worker"
-        val oldMissionWorker = workManager.getWorkInfosForUniqueWork(oldMissionWorkerName).get()
-        if (oldMissionWorker.isNotEmpty()) {
-            workManager.cancelUniqueWork(oldMissionWorkerName)
-        }
-
-        val oldContractWorkerName = "contract_widget_worker"
-        val oldContractWorker =
-            workManager.getWorkInfosForUniqueWork(oldContractWorkerName).get()
-        if (oldContractWorker.isNotEmpty()) {
-            workManager.cancelUniqueWork(oldContractWorkerName)
+        oldWorkNames.forEach { name ->
+            val worker = workManager.getWorkInfosForUniqueWork(name).get()
+            if (worker.isNotEmpty()) {
+                workManager.cancelUniqueWork(name)
+            }
         }
     }
 }
