0e2e22b82b535ebb8c62de85564862eab044c16c
mvdiener <mvdiener@gmail.com>
2025-06-09 11:32:35 -0700

    Add scheduler to onupdate

--- app/src/main/java/widget/WidgetReceiver.kt
+++ app/src/main/java/widget/WidgetReceiver.kt
@@ -1,11 +1,21 @@
 package widget
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
 abstract class WidgetReceiver : GlanceAppWidgetReceiver() {
     override fun onEnabled(context: Context) {
         super.onEnabled(context)
+        WidgetScheduler().scheduleUpdate(context)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
         WidgetScheduler().scheduleUpdate(context)
     }
 }
