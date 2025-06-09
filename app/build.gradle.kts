877141dd0528481246716655e99ae7411ea7c1a7
mvdiener <mvdiener@gmail.com>
2025-06-09 00:42:47 -0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.widgetegg.widgeteggapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.0.1"
+        versionCode = 15
+        versionName = "2.1.0"
 
         ndk {
             debugSymbolLevel = "SYMBOL_TABLE"
